a5e854072c8179b12cb69edfbebdb7312b259fc4
Edgardo Fernández <efernandeza@odybank.com.pe>
2023-08-23 21:32:06 -0500

    INICIO DE PROYECTO

---
+++ 20200100_20200175_20200200_HappyBirthday/app/src/main/java/com/example/a20200100_20200175_20200200_happybirthday/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.a20200100_20200175_20200200_happybirthday
+
+
+import androidx.compose.foundation.Image
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a20200100_20200175_20200200_happybirthday.ui.theme._20200100_20200175_20200200_HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            _20200100_20200175_20200200_HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background){
+                    GreetingImage(stringResource(R.string.fisi_text), "FISI")
+                }
+
+
+            }
+        }
+    }
+}
+
+
+
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center,modifier = modifier.padding(8.dp)) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box{
+        Image(
+            painter = image, contentDescription = null, contentScale = ContentScale.Crop, alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    _20200100_20200175_20200200_HappyBirthdayTheme {
+        GreetingImage("Kevin, Edgardo y Leonel", from = "FISI")
+    }
+}
+
+
---
+++ 20200100_20200175_20200200_HappyBirthday/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "20200100_20200175_20200200_HappyBirthday"
+include(":app")
+ 
